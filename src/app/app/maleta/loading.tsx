7d4df3953107e838ebbc338d08d02b08465db5f2
Wesley Hudson <monarcasemijoyas@gmail.com>
2026-05-01 11:09:15 -0300

    refactor: usar Skeleton do shadcn/ui nos loading files

--- src/app/app/maleta/loading.tsx
+++ src/app/app/maleta/loading.tsx
@@ -1,15 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function MaletaLoading() {
   return (
     <div className="flex flex-col min-h-full bg-[#F5F2EF]">
-      {/* Header skeleton */}
-      <div className="flex flex-col pt-6 pb-4 px-5">
-        <div className="flex items-center gap-4">
-          <div className="w-6 h-6 rounded bg-[#E8E2D6] animate-pulse" />
-          <div className="h-4 w-40 rounded bg-[#E8E2D6] animate-pulse" />
-        </div>
+      {/* Header */}
+      <div className="flex items-center gap-4 pt-6 pb-4 px-5">
+        <Skeleton className="w-6 h-6" />
+        <Skeleton className="h-4 w-40" />
       </div>
 
-      {/* Cards skeleton */}
+      {/* Cards */}
       <div className="flex flex-col gap-3 px-5">
         {[1, 2, 3].map((i) => (
           <div
@@ -18,12 +18,12 @@ export default function MaletaLoading() {
             style={{ boxShadow: "0 1px 4px rgba(0,0,0,0.04)" }}
           >
             <div className="flex justify-between items-center">
-              <div className="h-4 w-28 rounded bg-[#E8E2D6] animate-pulse" />
-              <div className="h-5 w-16 rounded-full bg-[#E8E2D6] animate-pulse" />
+              <Skeleton className="h-4 w-28" />
+              <Skeleton className="h-5 w-16 rounded-full" />
             </div>
             <div className="flex justify-between">
-              <div className="h-3 w-24 rounded bg-[#E8E2D6] animate-pulse" />
-              <div className="h-3 w-20 rounded bg-[#E8E2D6] animate-pulse" />
+              <Skeleton className="h-3 w-24" />
+              <Skeleton className="h-3 w-20" />
             </div>
           </div>
         ))}
